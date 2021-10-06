883ab51bcaa42404f88c54cf4eead7be141eee77
GuilleGorines <guillermo.gorines@gmail.com>
2021-10-06 13:21:24 +0200

    started change to accept file instead of string

--- bin/download_reference.py
+++ bin/download_reference.py
@@ -5,12 +5,16 @@ if [ $# -eq 2 ]
         assembly_summary_file='/processing_Data/bioinformatics/references/bacteria/latest_db/assembly_summary_bacteria.txt'
         bacterial_id=$1
         output_dir=$2
+
         nucleotide_end='_genomic.fna.gz'
         protein_end='_protein.faa.gz'
         gff_end='_genomic.gff.gz'
+
         ftp_path=$(cat $assembly_summary_file | grep "$bacterial_id" | cut -f20)
         ftp_path=$ftp_path/$bacterial_id
+
         cd $output_dir
+
         wget $ftp_path$nucleotide_end
         wget $ftp_path$protein_end
         wget $ftp_path$gff_end
@@ -27,27 +31,38 @@ import os
 import wget
 
 def parse_args(args=None):
-    Description = 'download the reference files (fna, faa, gff) from the reference ncbi file.'
-    Epilog = """Example usage: python download_reference.py -url <ftp path> -out_dir <output directory>"""
+    Description = 'download the reference files (fna, faa, gff) from the reference NCBI file.'
+    Epilog = """Example usage: python download_reference.py -file <file with the references> -out_dir <output directory>"""
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
-    parser.add_argument('-url', help="url path to download the query reference")
+    parser.add_argument('-file', help="file containing the path for download.")
     parser.add_argument('-out_dir', help="Output directory.")
+    
     return parser.parse_args(args)
 
-def download_references (url, out_dir):
+def download_references (path, out_dir):
     reference_ends = ['_genomic.fna.gz','_protein.faa.gz', '_genomic.gff.gz']
+    
+    with open(path) as infile:
+        infile = infile.readlines()
+        infile = infile[0]
+    
     os.mkdir(out_dir)
-    reference_file = url.split('/')[-1]
+    reference_file = infile.split('/')[-1]
+    
     for r_end in reference_ends:
+        
         out_file = os.path.join(out_dir,reference_file + r_end)
         file_url = url + '/' + reference_file + r_end
+        
         wget.download(file_url, out_file)
     return
 
 def main(args=None):
     args = parse_args(args)
-    download_references(args.url, args.out_dir)
+
+    download_references(args.file, args.out_dir)
+
 
 
 
